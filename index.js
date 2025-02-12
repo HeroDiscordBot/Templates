fa4f33bb2e07ea43c11dc9539a851c89b92069ff
HeroDiscordBot <copperprogrammed@gmail.com>
2025-02-12 14:24:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function addBotToDiscord1() {
+    window.open("https://discord.com/oauth2/authorize?client_id=1335695462753370133&permissions=8&integration_type=0&scope=bot+applications.commands");
+}
+
+function addBotToDiscord2() {
+    window.open("https://discord.com/oauth2/authorize?client_id=1335695462753370133&permissions=8&integration_type=0&scope=bot+applications.commands");
+}
+
+function addBotToDiscord3() {
+    window.open("https://discord.com/oauth2/authorize?client_id=1335695462753370133&permissions=8&integration_type=0&scope=bot+applications.commands");
+}
